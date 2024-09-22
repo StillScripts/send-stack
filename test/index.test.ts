45984469a470d3e1147f6f47a4df2fe58d888824
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-09-22 10:05:00 +1000

    Setup basic tests

---
+++ test/index.test.ts
@@ -0,0 +1,13 @@
+// test/index.test.ts
+import { app } from "@/app/(server)/server";
+import { describe, expect, it } from "bun:test";
+
+describe("Elysia", () => {
+  it("return a response", async () => {
+    const response = await app
+      .handle(new Request("http://localhost/api"))
+      .then((res) => res.text());
+
+    expect(response).toBe("hello Next");
+  });
+});
