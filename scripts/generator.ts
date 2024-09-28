ee3c01724938dc7f75d7428a6c678283883dd347
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-09-29 09:16:41 +1000

    Get started on enabling frontend script

--- scripts/generator.ts
+++ scripts/generator.ts
@@ -3,15 +3,10 @@ import path from 'path'
 
 import { controllerTemplate, routerTemplate } from './generator/templates'
 
-// Function to check if a model exists in the schema file
-async function checkTable(modelName: string) {
+async function checkModelExists(modelName: string) {
 	try {
 		const schemaContent = await fs.readFile('db/schema.ts', 'utf-8')
-		const regex = new RegExp(
-			`export\\s+const\\s+${modelName}\\s*=\\s*sqliteTable\\(\\s*"${modelName}"`,
-			'i'
-		)
-		return regex.test(schemaContent)
+		return schemaContent.includes(`export const ${modelName} = sqliteTable`)
 	} catch (error) {
 		console.error('Error reading schema file:', error)
 		return false
@@ -32,7 +27,6 @@ export async function generateRouterFile(
 	}
 }
 
-// Function to generate a new route file based on a model name
 export async function generateControllerFile(
 	modelName: string,
 	basePath = 'app/(server)/controllers'
@@ -49,17 +43,22 @@ export async function generateControllerFile(
 // Check if the model exists and then generate the route file
 const run = async () => {
 	// Extract the model name from the command line argument
-	const modelName = process.argv[2]?.replace('-', '')
+	const option = process.argv[2]
+	const modelName = process.argv[3]?.replace('-', '')
 
 	if (!modelName) {
 		console.error('Please provide a model name to check.')
 		process.exit(1)
 	}
 
-	const modelExists = await checkTable(modelName)
+	const modelExists = await checkModelExists(modelName)
 	if (modelExists) {
-		await generateControllerFile(modelName)
-		await generateRouterFile(modelName)
+		if (option === 'backend') {
+			await generateControllerFile(modelName)
+			await generateRouterFile(modelName)
+		} else {
+			console.log('Generating frontend')
+		}
 	} else {
 		console.log(`Model "${modelName}" does not exist in schema.`)
 	}
