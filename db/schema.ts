049498417b9e6491527cea82131eb8f7a6ea24ac
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-10-06 16:30:30 +1100

    Update schema with basic blog and user table

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm'
+import { relations, sql } from 'drizzle-orm'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 const createdAndUpdated = {
@@ -11,9 +11,38 @@ const createdAndUpdated = {
 		.notNull()
 }
 
+export const users = sqliteTable('users', {
+	id: integer('id').primaryKey(),
+	name: text('name'),
+	email: text('email').notNull().unique(),
+	passwordHash: text('password_hash').notNull(),
+	role: text('role').notNull().default('member'),
+	...createdAndUpdated,
+	deletedAt: integer('deleted_at', { mode: 'number' })
+})
+
+export const blogs = sqliteTable('blogs', {
+	id: integer('id').primaryKey(),
+	title: text('title'),
+	content: text('content'),
+	userId: integer('user_id').references(() => users.id),
+	...createdAndUpdated
+})
+
 export const movies = sqliteTable('movies', {
 	id: integer('id').primaryKey(),
 	title: text('name'),
 	releaseYear: integer('release_year'),
 	...createdAndUpdated
 })
+
+export const blogsRelations = relations(blogs, ({ one }) => ({
+	user: one(users, {
+		fields: [blogs.userId],
+		references: [users.id]
+	})
+}))
+
+export const usersRelations = relations(users, ({ many }) => ({
+	blogs: many(blogs)
+}))
