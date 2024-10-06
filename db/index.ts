8572a34815069071127212442f2c61f78409a6c8
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-10-07 00:41:46 +1100

    Get tests working with test Turso DB

--- db/index.ts
+++ db/index.ts
@@ -6,8 +6,20 @@ export const DB_URL =
 	process.env.NODE_ENV === 'test' ? 'test-sqlite.db' : 'sqlite.db'
 
 config({ path: '.env' })
-const client = createClient({
-	url: process.env.TURSO_CONNECTION_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN!
-})
+
+function getDbConfig() {
+	if (process.env.NODE_ENV === 'test') {
+		return {
+			url: process.env.TEST_TURSO_CONNECTION_URL!,
+			authToken: process.env.TEST_TURSO_AUTH_TOKEN!
+		}
+	}
+	return {
+		url: process.env.TURSO_CONNECTION_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!
+	}
+}
+
+const dbConfig = getDbConfig()
+const client = createClient(dbConfig)
 export const db = drizzle(client)
