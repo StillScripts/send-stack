8d5b6311ade151cf0acd9c974193e71b4d63d048
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-10-07 09:25:25 +1100

    Setup basic blog page

--- app/(site)/blog/[id]/page.tsx
+++ app/(site)/blog/[id]/page.tsx
@@ -25,7 +25,30 @@ const EditBlog = async ({ params }: { params: { id: string } }) => {
 		notFound()
 	}
 
-	return <div>{data.title}</div>
+	return (
+		<div className="prose mt-16">
+			{data.content?.map((element, index) => {
+				switch (element.type) {
+					case 'h1':
+						return <h1 key={index}>{element.text}</h1>
+					case 'h2':
+						return <h2 key={index}>{element.text}</h2>
+					case 'h3':
+						return <h3 key={index}>{element.text}</h3>
+					case 'h4':
+						return <h4 key={index}>{element.text}</h4>
+					case 'h5':
+						return <h5 key={index}>{element.text}</h5>
+					case 'h6':
+						return <h6 key={index}>{element.text}</h6>
+					case 'paragraph':
+						return <p key={index}>{element.text}</p>
+					default:
+						return null
+				}
+			})}
+		</div>
+	)
 }
 
 export default EditBlog
